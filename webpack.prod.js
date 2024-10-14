6db0bfecfead1639af165c40c61953e423ffeee7
BrizuelaFederico <brizuela.federicoivan@gmail.com>
2024-10-14 13:11:04 -0300

    webpack: added MiniCssExtractPlugin, CssMinimizerPlugin and postcss-loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,46 @@
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
 export default merge(common, {
   mode: "production",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
 });
